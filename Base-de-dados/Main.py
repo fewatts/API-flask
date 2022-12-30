28ebadc23054496cd5a8a49f0fd60e8bbd17edee
Fernando Alves de Paula <feralveswatts@gmail.com>
2022-12-30 17:59:59 -0300

    Update-APIflask

--- Base-de-dados/Main.py
+++ Base-de-dados/Main.py
@@ -1,19 +1,27 @@
 from flask import Flask, make_response, jsonify, request
 from BD import Carros 
 app = Flask(__name__)
-
+app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/carros', methods=['GET'])
 def get_carros():
     return make_response(
-        jsonify(Carros)
+        jsonify(
+            message='Lista de carros.',
+            Dados=Carros
+        )
     )
 
 
 @app.route('/carros', methods=['POST'])
 def create_carro():
     carro = request.json
-    return carro
-
+    Carros.append(carro)
+    return make_response(
+        jsonify(
+            message='Carro cadastrado!.',
+            Carro_cadastrado=carro
+        )
+    )
 
 app.run()
