7f930b2ae09f05c7537a7805acda37da61720f2b
Fernando Alves de Paula <feralveswatts@gmail.com>
2022-12-30 16:31:10 -0300

    Update Main.py

--- Base-de-dados/Main.py
+++ Base-de-dados/Main.py
@@ -1,11 +1,19 @@
-from flask import Flask
-from bd import Carros 
-
+from flask import Flask, make_response, jsonify, request
+from BD import Carros 
 app = Flask(__name__)
 
 
+@app.route('/carros', methods=['GET'])
 def get_carros():
-    return Carros
+    return make_response(
+        jsonify(Carros)
+    )
+
+
+@app.route('/carros', methods=['POST'])
+def create_carro():
+    carro = request.json
+    return carro
 
 
 app.run()
