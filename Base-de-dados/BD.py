bc45a3a27376a36a925ef0ab9313c2890ff094ff
Fernando Alves de Paula <feralveswatts@gmail.com>
2022-12-30 16:09:40 -0300

    update

---
+++ Base-de-dados/BD.py
@@ -0,0 +1,72 @@
+Carros = [
+    {
+        'ID': 1,
+        'Marca': 'Mitsubishi',
+        'Modelo': 'Triton',
+        'Ano': '2022',
+        'Preço': 311990
+    },
+    {
+        'ID': 2,
+        'Marca': 'Audi',
+        'Modelo': 'Q3 Performace',
+        'Ano': '2022',
+        'Preço':321990
+    },
+    {
+        'ID': 3,
+        'Marca': 'BMW',
+        'Modelo': 'X1 Drive 20i Sport Plus',
+        'Ano': '2022',
+        'Preço':330950
+    },
+    {
+        'ID': 4,
+        'Marca': 'BMW',
+        'Modelo': '320i M sport',
+        'Ano': '2022',
+        'Preço': 339950
+    },
+    {
+        'ID': 5,
+        'Marca': 'Audi',
+        'Modelo': 'Q3 Sportback Performace',
+        'Ano': '2022',
+        'Preço':347990
+    },
+    {
+        'ID': 6,
+        'Marca': 'Chevrolet',
+        'Modelo': 'Trailblazer Premier',
+        'Ano': '2022',
+        'Preço': 367690
+    },
+    {
+        'ID': 7,
+        'Marca': 'Land Rover',
+        'Modelo': 'Discovery Sport',
+        'Ano': '2022',
+        'Preço': 413950
+    },
+    {
+        'ID': 8,
+        'Marca': 'Range Rover',
+        'Modelo': 'Evoque R-Dinamic',
+        'Ano': '2022',
+        'Preço': 446950
+    },
+    {
+        'ID': 9,
+        'Marca': 'BMW',
+        'Modelo': 'X3 M40i',
+        'Ano': '2022',
+        'Preço': 589950
+    },
+    {
+        'ID': 10,
+        'Marca': 'BMW',
+        'Modelo': 'X4 M40i',
+        'Ano': '2022',
+        'Preço': 609950
+    }
+]
---
+++ Base-de-dados/Main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from bd import Carros 
+
+app = Flask(__name__)
+
+
+def get_carros():
+    return Carros
+
+
+app.run()
